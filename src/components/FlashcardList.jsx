63bbb925acbb6c4b93253fabaee46836dd138fc4
Aryan7523 <aryannikam713@gmail.com>
2025-06-19 12:56:01 +0530

    Update FlashcardList.jsx

--- src/components/FlashcardList.jsx
+++ src/components/FlashcardList.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Flashcard from './Flashcard';
 
-const FlashcardList = ({ flashcards, onDelete }) => {
+const FlashcardList = ({ flashcards, onDelete, onEdit }) => {
   return (
     <div className="flashcard-list">
       {flashcards.map((card) => (
-        <Flashcard key={card.id} flashcard={card} onDelete={onDelete} />
+        <Flashcard
+          key={card.id}
+          flashcard={card}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
       ))}
     </div>
   );
